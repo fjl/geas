af6742c49c67b11734f606b6a0ba71f3530dd354
Felix Lange <fjl@twurst.com>
2025-10-19 23:33:56 +0200

    asm: simplify unused label analysis

--- asm/compiler_analysis.go
+++ asm/compiler_analysis.go
@@ -24,33 +24,14 @@ import (
 
 // checkLabelsUsed warns about label definitions that were not hit by the evaluator.
 func (c *Compiler) checkLabelsUsed(prog *compilerProg, e *evaluator) {
-	// Gather documents referenced by program.
-	var docs []*ast.Document
-	docset := make(set.Set[*ast.Document])
-	macroset := make(set.Set[*ast.InstructionMacroDef])
-	for section := range prog.iterSections() {
-		// Ensure to walk macroexpansions only once.
-		if section.macroArgs != nil {
-			if macroset.Includes(section.macroArgs.def) {
-				continue
-			}
-			macroset.Add(section.macroArgs.def)
+	seen := make(set.Set[*ast.LabelDefSt])
+	for _, l := range prog.labels {
+		if seen.Includes(l.def) {
+			continue
 		}
-		if !docset.Includes(section.doc) {
-			docset.Add(section.doc)
-			docs = append(docs, section.doc)
-		}
-	}
-
-	// Check against evaluator.
-	for _, doc := range docs {
-		for _, st := range doc.Statements {
-			switch st := st.(type) {
-			case *ast.LabelDefSt:
-				if !e.isLabelUsed(st) {
-					c.warnf(st, "label %s unused in program", st)
-				}
-			}
+		seen.Add(l.def)
+		if !e.isLabelUsed(l.def) {
+			c.warnf(l.def, "label %s unused in program", l.def)
 		}
 	}
 }
